0e094f2216a9cb1ab78e942e4440e366a9d54817
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-10-07 17:35:38 +0300

    add connection for db

--- src/Controller/Site/About.php
+++ src/Controller/Site/About.php
@@ -2,13 +2,28 @@
 
 namespace Controller\Site;
 
+use Beauty\Request;
+use FlashLogger;
+
 class About extends Base
 {
+    /**
+     * @var \DB
+     */
+    private $db;
+
+    public function __construct(Request $request, FlashLogger\ProjectList $projectList, \DB $db)
+    {
+        parent::__construct($request, $projectList);
+        $this->db = $db;
+    }
+
     /**
      * @return \Beauty\Http\ResponseInterface
      */
     public function aboutAction()
     {
+        var_dump($this->db->query('select 1'));
         return $this->render('about');
     }
 }
